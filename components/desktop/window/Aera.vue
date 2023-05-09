19a7be5f4db7049e6f728b8f958c16b909b35fa0
zheng <2642198391@qq.com>
2023-05-09 20:26:42 +0800

    app窗口

---
+++ components/desktop/window/Aera.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app-aera">
+        <AppVscode v-if="isVscodeShow"/>
+        <AppChatgpt v-if="isChatgptShow"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {useAppsStore} from '~/store/Apps/apps'
+const appStore = useAppsStore()
+const isVscodeShow = computed(()=>appStore.isOpenApp['vscode'])
+const isChatgptShow = computed(()=>appStore.isOpenApp['chatgpt'])
+</script>
+
+<style scoped>
+.app-aera{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+</style>
