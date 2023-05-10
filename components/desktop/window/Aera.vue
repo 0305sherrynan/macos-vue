ac5afa8d317314c7cd45c333003df3a881425b13
zheng <2642198391@qq.com>
2023-05-10 09:07:20 +0800

    remove flex

--- components/desktop/window/Aera.vue
+++ components/desktop/window/Aera.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app-aera">
-        <AppVscode v-if="isVscodeShow"/>
+        <AppVscode v-if="isVscodeShow"  />
         <AppChatgpt v-if="isChatgptShow"/>
     </div>
 </template>
@@ -10,13 +10,14 @@ import {useAppsStore} from '~/store/Apps/apps'
 const appStore = useAppsStore()
 const isVscodeShow = computed(()=>appStore.isOpenApp['vscode'])
 const isChatgptShow = computed(()=>appStore.isOpenApp['chatgpt'])
+
 </script>
 
 <style scoped>
 .app-aera{
-    display: flex;
+    /* display: flex;
     flex-direction: row;
     justify-content: center;
-    align-items: center;
+    align-items: center; */
 }
 </style>
