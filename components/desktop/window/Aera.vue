ae69da1793a748b9e98f3cd0c9ce06fb21d65b3b
zheng <2642198391@qq.com>
2023-05-10 11:13:28 +0800

    add animation to vscodeApp

--- components/desktop/window/Aera.vue
+++ components/desktop/window/Aera.vue
@@ -1,28 +1,56 @@
 <template>
     <div class="app-aera">
-        <div class="inline-block relative" >
-             <AppVscode   v-if="isVscodeShow" class="vscode-app"/> 
-            <AppChatgpt v-if="isChatgptShow"/>
-            
+        <div class="inline-block relative">
+            <Transition name="vscode">
+                <AppVscode v-if="isVscodeShow" class="vscode-app" @click="focuEvent('vscode')" />
+            </Transition>
+            <AppChatgpt v-if="isChatgptShow" @click="focuEvent('chatgpt')" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import {useAppsStore} from '~/store/Apps/apps'
+import { useAppsStore } from '~/store/Apps/apps'
+import { appsID } from '~/configs/apps/apps.config'
 const appStore = useAppsStore()
-const isVscodeShow = computed(()=>appStore.isOpenApp['vscode'])
-const isChatgptShow = computed(()=>appStore.isOpenApp['chatgpt'])
+const isVscodeShow = computed(() => appStore.isOpenApp['vscode'])
+const isChatgptShow = computed(() => appStore.isOpenApp['chatgpt'])
 
+/**
+ * 
+ */
+
+const focuEvent = (activeID: appsID) => {
+    appStore.activeAppInstance = activeID
+    console.log(activeID)
+}
 </script>
 
 <style scoped lang="scss">
-.app-aera{
-     display: flex;
+.vscode-enter-from{
+    transform:scale(0,0);
+    transform-origin: center,center;
+    opacity: 0;
+}
+.vscode-enter-active{
+    transition: 300ms linear;
+}
+.vscode-leave-active{
+    transition: 300ms linear;
+}
+.vscode-leave-to{
+    
+    transform:scale(0,0);
+    transform-origin: center,center;
+    opacity: 0;
+}
+.app-aera {
+    display: flex;
     flex-direction: row;
     justify-content: center;
-    align-items: center; 
-    .vscode-app{
+    align-items: center;
+
+    .vscode-app {
         position: fixed;
     }
 }
