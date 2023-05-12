67e251011e8e1b403fdc940a8b3ca1a7a7539bb2
zheng <2642198391@qq.com>
2023-05-12 16:36:57 +0800

    各个控件

---
+++ components/controlcentre/FullscreenItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="wifi-item" @click="isOpen = !isOpen">
+            <svg v-if="!isOpen" t="1683806103011" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="3549" width="17" height="17">
+                <path
+                    d="M358.4 768H426.666667v85.333333H213.333333v-213.333333h85.333334v68.266667l128-128 59.733333 59.733333-128 128z m345.6 0l-128-128 59.733333-59.733333 132.266667 132.266666V640h85.333333v213.333333h-213.333333v-85.333333h64zM358.4 298.666667l128 128-59.733333 59.733333-128-128V426.666667H213.333333V213.333333h213.333334v85.333334H358.4z m345.6 0H640V213.333333h213.333333v213.333334h-85.333333V354.133333l-132.266667 132.266667-59.733333-59.733333 128-128z"
+                    fill="#444444" p-id="3550"></path>
+            </svg>
+            <svg v-if="isOpen" t="1683806103011" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="3549" width="17" height="17">
+                <path
+                    d="M358.4 768H426.666667v85.333333H213.333333v-213.333333h85.333334v68.266667l128-128 59.733333 59.733333-128 128z m345.6 0l-128-128 59.733333-59.733333 132.266667 132.266666V640h85.333333v213.333333h-213.333333v-85.333333h64zM358.4 298.666667l128 128-59.733333 59.733333-128-128V426.666667H213.333333V213.333333h213.333334v85.333334H358.4z m345.6 0H640V213.333333h213.333333v213.333334h-85.333333V354.133333l-132.266667 132.266667-59.733333-59.733333 128-128z"
+                    fill="#ffffff" p-id="3550"></path>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref<boolean>(false)
+watch(isOpen, (newval, oldval) => {
+    console.log(newval)
+})
+const currentColor = computed(() => isOpen.value ? 'var(--control-color-open)' : 'var(--control-color-closed)')
+watch(currentColor, (newval, oldval) => {
+    console.log(newval)
+})
+</script>
+
+<style scoped>
+.wifi-item {
+    /* line-height: 1rem; */
+    border-radius: 50%;
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: v-bind(currentColor);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}</style>
+  
+
+  
+
---
+++ components/controlcentre/WifiItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="wifi-item" @click="isOpen = !isOpen">
+            <svg v-show="!isOpen" t="1683797462359" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="2746" width="17" height="17">
+                <path
+                    d="M0 352.832l93.12 98.752c231.296-245.44 606.464-245.44 837.76 0L1024 352.832C741.44 53.056 283.008 53.056 0 352.832z m372.352 395.008L512 896l139.648-148.16c-76.8-81.92-202.048-81.92-279.296 0zM186.24 550.4l93.12 98.752c128.448-136.32 336.96-136.32 465.408 0L837.824 550.4c-179.648-190.592-471.488-190.592-651.648 0z"
+                    fill="#2c2c2c" p-id="2747"></path>
+            </svg>
+            <svg v-show="isOpen" t="1683797462359" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                p-id="2746" width="17" height="17">
+                <path
+                    d="M0 352.832l93.12 98.752c231.296-245.44 606.464-245.44 837.76 0L1024 352.832C741.44 53.056 283.008 53.056 0 352.832z m372.352 395.008L512 896l139.648-148.16c-76.8-81.92-202.048-81.92-279.296 0zM186.24 550.4l93.12 98.752c128.448-136.32 336.96-136.32 465.408 0L837.824 550.4c-179.648-190.592-471.488-190.592-651.648 0z"
+                    fill="#ffffff" p-id="2747"></path>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = ref<boolean>(false)
+watch(isOpen, (newval, oldval) => {
+    console.log(newval)
+})
+const currentColor = computed(() => isOpen.value ? 'var(--control-color-open)' : 'var(--control-color-closed)')
+watch(currentColor, (newval, oldval) => {
+    console.log(newval)
+})
+</script>
+
+<style scoped>
+.wifi-item {
+    /* line-height: 1rem; */
+    border-radius: 50%;
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: v-bind(currentColor);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+  
+
+  
+
