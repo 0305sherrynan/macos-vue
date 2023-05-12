84f74464f93cb24623c44a61efaadb3808c920f1
zheng <2642198391@qq.com>
2023-05-12 17:41:44 +0800

    加入slider

---
+++ components/controlcentre/Lightslider.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <v-slider v-model="slider1" color="orange" label="color"> </v-slider>
+    </div>
+</template>
+
+<script setup lang="ts">
+const slider1 = ref<number>(0)
+</script>
+
+<style scoped></style>
