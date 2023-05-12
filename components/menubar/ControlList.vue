7489457ba091ae1ca45a7fe5b5eab516444e40e9
zheng <2642198391@qq.com>
2023-05-12 16:36:44 +0800

    右端控件菜单

---
+++ components/menubar/ControlList.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="all-controls">
+        <div class="first-controls">
+            <div class="single-contrl">
+                <ControlcentreWifiItem />
+                <div class="control-span">
+                    <span>WiFi</span><br>
+                    <span>on</span>
+                </div>
+            </div>
+            <div class="single-contrl">
+                <ControlcentreBluetoothItem />
+                <div class="control-span">
+                    <span>Bluetooth</span><br>
+                    <span>off</span>
+                </div>
+            </div>
+            <div class="single-contrl">
+                <ControlcentreFullscreenItem />
+                <div class="control-span">
+                    <span>FullScreen</span><br>
+                    <span>off</span>
+                </div>
+            </div>
+        </div>
+        <div class="left-top-controls">
+            <div class="single-contrl second-controls">
+                <ControlcentreDarkmodeItem />
+                <div class="control-span single-span">
+                    <span>DarkMode</span>
+                </div>
+            </div>
+            <div class="single-contrl second-controls">
+                <ControlcentreEyeproItem />
+                <div class="control-span single-span">
+                    <span>DarkMode</span>
+                </div>
+            </div>
+        </div>
+
+
+        
+        <!-- <Control> -->
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.all-controls {
+    display: flex;
+    flex-direction: row;
+}
+.first-controls{
+    border-radius: .8em;
+    background-color: hsla(0, 0%, 100%, .4);
+    /* box-sizing: border-box; */
+    border: 1px solid #e5e7eb;
+}
+.single-contrl {
+    
+    line-height: 1rem;
+    display: flex;
+    flex-direction: row;
+    padding: .2rem;
+
+    &:not(:nth-child(1)) {
+        margin-top: .3rem;
+    }
+
+    .control-span {
+        height: 2.5rem;
+        font-size: 1rem;
+        margin-left: .7rem;
+    }
+}
+.second-controls {
+    padding: 1rem;
+    vertical-align: middle;
+    border-radius: .8em;
+    background-color: hsla(0, 0%, 100%, .4);
+    /* box-sizing: border-box; */
+    border: 1px solid #e5e7eb;
+}
+
+.single-span {
+    display: inline-block;
+    height: 2.5rem;
+    font-size: 1rem;
+    line-height: 2.5rem;
+}
+</style>
---
+++ components/menubar/RightBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="relative">
+        <div class="arrow"  @click="rotate = '180deg'" ref="arrowRef"></div>
+        <div class="absolute top-10">
+            <MenubarControlList/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
+const rotate = ref<string>('')
+const arrowRef = ref<HTMLElement>()
+const clickOut = ()=>{
+    rotate.value = '0deg'
+}
+onClickOutside(arrowRef, clickOut)
+</script>
+
+<style scoped>
+.arrow {
+    /* border-top: 50px solid white; */
+    width: 0px;
+    height: 0px;
+    
+    border-bottom: .6rem solid  rgba(0,0,0,.5);
+    border-left: .6rem solid transparent;
+    border-right: .6rem solid transparent;
+    transition: all 300ms linear;
+    transform:rotate(v-bind(rotate));
+
+}
+</style>
