222e8014c3db2abe684b89f65f3afda87b250448
zheng <2642198391@qq.com>
2023-05-12 17:41:54 +0800

    更改布局

--- components/menubar/ControlList.vue
+++ components/menubar/ControlList.vue
@@ -1,66 +1,73 @@
 <template>
     <div class="all-controls">
-        <div class="first-controls">
-            <div class="single-contrl">
-                <ControlcentreWifiItem />
-                <div class="control-span">
-                    <span>WiFi</span><br>
-                    <span>on</span>
+        <div class="top-controls">
+            <div class="first-controls">
+                <div class="single-contrl">
+                    <ControlcentreWifiItem />
+                    <div class="control-span">
+                        <span>WiFi</span><br>
+                        <span>on</span>
+                    </div>
+                </div>
+                <div class="single-contrl">
+                    <ControlcentreBluetoothItem />
+                    <div class="control-span">
+                        <span>Bluetooth</span><br>
+                        <span>off</span>
+                    </div>
+                </div>
+                <div class="single-contrl">
+                    <ControlcentreFullscreenItem />
+                    <div class="control-span">
+                        <span>FullScreen</span><br>
+                        <span>off</span>
+                    </div>
                 </div>
             </div>
-            <div class="single-contrl">
-                <ControlcentreBluetoothItem />
-                <div class="control-span">
-                    <span>Bluetooth</span><br>
-                    <span>off</span>
+            <div class="left-top-controls">
+                <div class="single-contrl second-controls">
+                    <ControlcentreDarkmodeItem />
+                    <div class="control-span single-span">
+                        <span>DarkMode</span>
+                    </div>
                 </div>
-            </div>
-            <div class="single-contrl">
-                <ControlcentreFullscreenItem />
-                <div class="control-span">
-                    <span>FullScreen</span><br>
-                    <span>off</span>
+                <div class="single-contrl second-controls">
+                    <ControlcentreEyeproItem />
+                    <div class="control-span single-span">
+                        <span>DarkMode</span>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="left-top-controls">
-            <div class="single-contrl second-controls">
-                <ControlcentreDarkmodeItem />
-                <div class="control-span single-span">
-                    <span>DarkMode</span>
-                </div>
-            </div>
-            <div class="single-contrl second-controls">
-                <ControlcentreEyeproItem />
-                <div class="control-span single-span">
-                    <span>DarkMode</span>
-                </div>
-            </div>
+        <div>
+            <ControlcentreLightslider />
         </div>
-
-
-        
-        <!-- <Control> -->
     </div>
 </template>
 
 <script setup lang="ts">
-
+const slider1 = ref<number>(0)
 </script>
 
 <style scoped lang="scss">
-.all-controls {
+.all-controls{
+    display: flex;
+    flex-direction: column;
+}
+.top-controls {
     display: flex;
     flex-direction: row;
 }
-.first-controls{
+
+.first-controls {
     border-radius: .8em;
     background-color: hsla(0, 0%, 100%, .4);
     /* box-sizing: border-box; */
     border: 1px solid #e5e7eb;
 }
+
 .single-contrl {
-    
+
     line-height: 1rem;
     display: flex;
     flex-direction: row;
@@ -76,6 +83,7 @@
         margin-left: .7rem;
     }
 }
+
 .second-controls {
     padding: 1rem;
     vertical-align: middle;
@@ -90,5 +98,4 @@
     height: 2.5rem;
     font-size: 1rem;
     line-height: 2.5rem;
-}
-</style>
+}</style>
