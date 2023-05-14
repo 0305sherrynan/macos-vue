1da4de2581d5f77b1e571cce177e7ea1cb36e89a
zheng <2642198391@qq.com>
2023-05-14 14:33:42 +0800

    主题+控件store

---
+++ store/themes/themes.ts
@@ -0,0 +1,26 @@
+import { defineStore, StoreDefinition } from 'pinia'
+import {computed } from "vue"
+import {appsID} from '~/configs/apps/apps.config'
+interface themesAndControlIntf{
+    //是否为暗主题模式
+    dark:boolean,
+    brightness:number,
+    soundSize:number,
+
+
+}
+export const useThemesStore = defineStore('themes', {
+    state: ():themesAndControlIntf => ({
+        dark:false,
+        brightness:100,
+        soundSize:100
+    }),
+    getters:{
+        brightnessGetter:(state):ComputedRef=>computed(()=>state.brightness)
+    },
+    actions: {
+        setBrightness(value:number){
+            this.brightness = value
+        }
+    },
+  })
