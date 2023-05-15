b9f73081a703d53f18568962b79dcdfd05f79157
zheng <2642198391@qq.com>
2023-05-15 20:25:48 +0800

    配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,42 +3,42 @@
 import type { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-  buildModules: ['@nuxt/typescript-build','@nuxtjs/tailwindcss'],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss'],
   mode: 'universal',
-  modules:[
+  modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     '@vueuse/nuxt',
     '@invictus.codes/nuxt-vuetify',
     '@nuxt/devtools'
-],
-components:[
+  ],
+  components: [
     {
-        path:'~/components/',
-        pathPrefix:true  //设置为false 则不会通过路径名拼装
+      path: '~/components/',
+      pathPrefix: true  //设置为false 则不会通过路径名拼装
     }
-],   
-vite: {
+  ],
+  vite: {
     css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@use "@/assets/css/sass/global.scss" as *;'	// 注意文件路径要配成自己的
-            }
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/css/sass/global.scss" as *;'	// 注意文件路径要配成自己的
         }
+      }
     }
-},
-vuetify: {
+  },
+  vuetify: {
     /* vuetify options */
     vuetifyOptions: {
       // @TODO: list all vuetify options
     },
     moduleOptions: {
       /* nuxt-vuetify module options */
-      treeshaking: true ,
-      useIconCDN: true ,
+      treeshaking: true,
+      useIconCDN: true,
       /* vite-plugin-vuetify options */
-      styles: true  ,
-      autoImport: true ,
+      styles: true,
+      autoImport: true,
     }
   }
 }
