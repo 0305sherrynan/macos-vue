b6ae980466914ee8c941b6e7c85b04ba8c6ba250
zheng <2642198391@qq.com>
2023-05-16 16:57:11 +0800

    将动态背景封装为组件

---
+++ components/desktop/wall/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="wall-index" 
+        :style="{backgroundImage:'url(' + switchNewPaper + ')'}">
+       
+    </div>
+</template>
+
+<script setup lang="ts">
+import { url } from 'inspector';
+import {usePapersStore} from '~/store/Wallpaper/paper'
+const paperStore = usePapersStore()
+const switchNewPaper = ref<string>(paperStore.image)
+watch((paperStore.imageGetter),(newValu)=>{
+    console.log(12345)
+    switchNewPaper.value = newValu
+})
+</script>
+
+<style scoped>
+.wall-index {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    will-change: background-image;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    transition: background-image 100ms ease-in;
+}
+</style>
